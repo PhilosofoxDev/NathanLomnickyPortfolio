6dd13f862f1f13c7c8113719cedaf65d1fc25056
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-07-17 17:59:14 -0500

    tinkering

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Food from './about.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Food />
   </StrictMode>,
 )
