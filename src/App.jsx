45501bcd348fd0f8998de2dc080be70ac5d1d3b1
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-07-18 22:20:55 -0500

    End Day 7/18

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
       
-      <div>
-        <h1 className='bg-windowBox justify-center justify-items-center text-center'>Hello World</h1>
-      </div>
     </>
   )
 }
