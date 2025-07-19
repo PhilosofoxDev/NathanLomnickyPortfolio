c908c62000657a9b1005aad4b632607aff0a4c07
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-07-18 20:24:25 -0500

    Setup Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Food from './about.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Food />
+    <>
+    <App />
+    </>
   </StrictMode>,
 )
