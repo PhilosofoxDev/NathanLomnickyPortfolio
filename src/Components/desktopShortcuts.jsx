2e134206637ff8a5124d798dc889a7c36aee0e1b
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-07-23 09:33:52 -0500

    Finished Layout 7/22
    Not much time to work today, but the general layout is done. Needs interactivity and polish

--- src/Components/desktopShortcuts.jsx
+++ src/Components/desktopShortcuts.jsx
@@ -8,33 +8,58 @@ function Shortcuts() {
         <div class="flex w-35 absolute justify-center h-screen text-center font-dotoBold text-black text-md z-100"> {/*Left Bar (SHOULD BE ON TOP (highest z-order)*/}
             <div class="mt-2 mr-9"> {/*Left Icon Container*/}
                 <div>
-                    <img class="max-w-20" src={houseIcon} alt="HomeIcon"></img>
-                    <h1>Home</h1>
-                </div>
-                <div class="mt-2">      
-                    <img class="max-w-20" src={clipboardIcon} alt="AboutIcon"></img> {/*Add top margin to every element after first one*/}
-                    <h1>About</h1>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="HomeIcon"></img>
+                        <h1>Home</h1>
+                    </button>
                 </div>
                 <div class="mt-2">
-                    <img class="max-w-20" src={houseIcon} alt="FolderIcon"></img>
-                    <h1>Work</h1>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={clipboardIcon} alt="AboutIcon"></img> {/*Add top margin to every element after first one*/}
+                        <h1>About</h1>
+                    </button>
                 </div>
                 <div class="mt-2">
-                    <img class="max-w-20" src={houseIcon} alt="Envelope/Phone Icon"></img>
-                    <h1>Contact</h1>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="FolderIcon"></img>
+                        <h1>Work</h1>
+                    </button>
                 </div>
                 <div class="mt-2">
-                    <img class="max-w-20" src={houseIcon} alt="ChainlinkIcon"></img>
-                    <h1>Links</h1>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="Envelope/Phone Icon"></img>
+                        <h1>Contact</h1>
+                    </button>
                 </div>
                 <div class="mt-2">
-                    <img class="max-w-20" src={houseIcon} alt="RecycleBinIcon"></img>
-                    <h1>Misc</h1>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="ChainlinkIcon"></img>
+                        <h1>Links</h1>
+                    </button>
+                </div>
+                <div class="mt-2">
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="RecycleBinIcon"></img>
+                        <h1>Misc</h1>
+                    </button>
                 </div>
             </div>
         </div>
-        <div class="bg-amber-400 flex w-35 absolute justify-self-end justify-center h-screen text-center font-dotoBold text-black text-md z-100">
-            <h1>Right side icons go here &#40;Light/Dark mode switcher, mute/unmute, etc&#41;</h1> {/*Left Icon Container*/}
+        <div class="flex w-35 absolute justify-self-end justify-center h-screen text-center font-dotoBold text-black text-md z-100">
+            <div class="mt-2"> {/*Left Icon Container*/}
+                <div>
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={houseIcon} alt="HomeIcon"></img>
+                        <h1>Light</h1> {/*Maybe/Maybe not have text*/}
+                    </button>
+                </div>
+                <div class="mt-2">      
+                    <button class="hover:bg-blue-300">
+                        <img class="max-w-20" src={clipboardIcon} alt="AboutIcon"></img> {/*Add top margin to every element after first one*/}
+                        <h1>Mute</h1> {/*Maybed/Maybe not have text*/}
+                    </button>
+                </div>
+            </div>
         </div>
         </>
     )
