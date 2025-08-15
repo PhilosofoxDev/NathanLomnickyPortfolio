55492b851fc4cce6fecd88b2e909a471b0770a27
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-08-15 12:44:05 -0500

    About Window First Draft FINALLY DONEEEE
    Still gonna look over and revise but god I can finally move on to (hopefully) easier windows.

--- src/Components/aboutWindow.jsx
+++ src/Components/aboutWindow.jsx
@@ -26,7 +26,7 @@ export default function AboutWindow({ aboutOpen, aboutOnClose }) {
           </button>
         </div>
         {/*Introduction of About Window*/}
-        <div className="justify-center h-115 max-w-full max-h-115 text-wrap wrap-anywhere overflow-y-auto">
+        <div className="flex flex-col justify-center h-115 max-w-full max-h-115 text-wrap wrap-anywhere">
           <div className="flex font-outfit pt-10 pl-13 text-black">
             <img
               src={DuckPFP}
@@ -41,54 +41,127 @@ export default function AboutWindow({ aboutOpen, aboutOnClose }) {
                 Independant Game and Web Developer
               </h2>
               <h2 className="text-[1.5rem] font-outfitMedium">
-                Student at <a href="https://miamioh.edu/">Miami University</a>
+                Student at{" "}
+                <a href="https://miamioh.edu/" target="_blank">
+                  <span className="font-outfitMedium text-red-500 underline hover:text-red-700">
+                    Miami University
+                  </span>
+                </a>
               </h2>
             </div>
           </div>
           <hr className="mt-7 stroke-black opacity-20" />
           {/*Body of About Window*/}
-          <div className="text-[1.44rem] flex-col justify-between font-outfit ml-16 mr-10 mt-6 mb-15">
-            <h1>
-              Hey! I'm
-              <span className="text-green-700 font-outfitMedium"> Nathan</span>,
-              a Developer currently studying Game Design and Computer Science
-              at&nbsp;
-              <a href="https://miamioh.edu/" target="_blank">
-                <span className="font-outfitMedium text-red-500 underline hover:text-red-700">
-                  Miami University
-                </span>
-              </a>
-              .
-            </h1>
-            <h1 className="mt-9 font-outfit">Outside of class, I like to...</h1>
-            <ul className="list-disc ml-6 mt-5 font-outfitLight">
-              <li className="mt-4">
-                Create&nbsp;
-                <a href="https://philosofox.itch.io/" target="_blank">
-                  <span className="text-green-700 font-outfit underline hover:text-green-900">
-                    Video Games
+          {/*Summary*/}
+          <div className="overflow-y-auto flex-grow text-[1.44rem] flex-col justify-between font-outfit ml-16">
+            <div className="mt-6 mb-20 mr-7">
+              <h1>
+                Hey! I'm&nbsp;
+                <span className="text-green-700 font-outfitMedium">Nathan</span>
+                , a Developer currently studying Game Design and Computer
+                Science at&nbsp;
+                <a href="https://miamioh.edu/" target="_blank">
+                  <span className="font-outfitMedium text-red-500 underline hover:text-red-700">
+                    Miami University
                   </span>
                 </a>
-                &nbsp;on my own/with others
-              </li>
-              <li className="mt-1">
-                Develop Web Projects to assist myself creatively
-              </li>
-              <li className="mt-1">
-                <a
-                  href="https://www.youtube.com/@PhilosofoxDev"
-                  target="_blank"
-                >
-                  <span className="text-green-700 font-outfit underline hover:text-green-900">
-                    Stream and Create content
-                  </span>
-                </a>
-                &nbsp;around Games and Computer Science
-              </li>
-              <li className="mt-1">
-                Learn new development concepts online and through self-study
-              </li>
-            </ul>
+                .
+              </h1>
+              <div className="mt-8">
+                <h1 className="text-3xl font-outfitMedium">Summary:</h1>
+                <h1 className="font-outfit mt-2">
+                  Outside of class, I like to...
+                </h1>
+                <ul className="list-disc ml-6 mt-2 font-outfitLight">
+                  <li>
+                    Create&nbsp;
+                    <a href="https://philosofox.itch.io/" target="_blank">
+                      <span className="text-green-700 font-outfitMedium underline hover:text-green-900">
+                        Video Games
+                      </span>
+                    </a>
+                    &nbsp;on my own/with others
+                  </li>
+                  <li className="mt-1">
+                    Develop Web Projects to assist myself creatively
+                  </li>
+                  <li className="mt-1">
+                    <a
+                      href="https://www.youtube.com/@PhilosofoxDev"
+                      target="_blank"
+                    >
+                      <span className="text-green-700 font-outfitMedium underline hover:text-green-900">
+                        Stream and Create content
+                      </span>
+                    </a>
+                    &nbsp;around Games and Computer Science
+                  </li>
+                  <li className="mt-1">
+                    Learn new development concepts online and through self-study
+                  </li>
+                </ul>
+              </div>
+              {/*Development*/}
+              <div className="mt-10">
+                <h1 className="text-3xl font-outfitMedium">Development:</h1>
+                <h1 className="mt-2">
+                  I am currently focused on developing projects to help my
+                  academic and professional careers. This website is a prime
+                  example, and I hope to present myself here in professional and
+                  creative way.
+                </h1>
+                <h1 className="mt-3">
+                  On the game side of things, I am actively developing projects
+                  to feed my passions, learn different game development
+                  engines/techniques, and to diversify myself in the multitude
+                  of mediums game development requires.
+                </h1>
+              </div>
+              {/*Content*/}
+              <div className="mt-10">
+                <h1 className="text-3xl font-outfitMedium">
+                  Content Creation:
+                </h1>
+                <h1 className="mt-2">
+                  Under the name&nbsp;
+                  <a href="https://philosofoxdev.carrd.co/" target="_blank">
+                    <span className="text-orange-400 font-outfitBold underline hover:text-orange-500">
+                      Philosofox
+                    </span>
+                  </a>
+                  , I've begun to create videos online around my development
+                  journey. Content creation has frequently been a creative
+                  outlet for me, so combining it with my love for programming
+                  only seemed natural.
+                </h1>
+                <h1 className="mt-3">
+                  I previously made gaming content under the name&nbsp;
+                  <a href="https://www.youtube.com/@beefy1990" target="_blank">
+                    <span className="text-amber-700 font-outfitMedium underline hover:text-amber-800">
+                      Beefy
+                    </span>
+                  </a>
+                  .
+                </h1>
+              </div>
+              <div className="mt-10">
+                {/*Misc*/}
+                <h1 className="text-3xl font-outfitMedium">
+                  I'd love to talk more!
+                </h1>
+                <h1 className="mt-2">
+                  All of my info can be found by double clicking the "Links" and
+                  "Contact" icons. You can also directly email me by
+                  clicking&nbsp;
+                  <a href="mailto:NathanNikc7775@gmail.com" target="_blank">
+                    <span className="text-green-700 font-outfitMedium underline hover:text-green-900">
+                      here
+                    </span>
+                  </a>
+                  .
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
