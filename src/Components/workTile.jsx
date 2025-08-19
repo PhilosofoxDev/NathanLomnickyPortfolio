0b3cb693f362c7b75f82d47fb06be24c4e9274e0
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-08-18 21:40:29 -0500

    end of day 8/18
    Added more to work window. Optimized a bit. still having issues with the blender image lagging when it is first loaded.

--- src/Components/workTile.jsx
+++ src/Components/workTile.jsx
@@ -1,10 +1,14 @@
 import "tailwindcss";
 import React from "react";
 
-export default function WorkTile() {
+export default function WorkTile({ isOpen, children }) {
   return (
     <div
-      className={`transition-all duration-300 ease-in-out bg-workTileBg`}
-    ></div>
+      className={`transition-all duration-300 ease-in-out overflow-hidden ${
+        isOpen ? "max-h-[1000px] opacity-100 mt-5" : "max-h-0 opacity-0"
+      }`}
+    >
+      {children}
+    </div>
   );
 }
--- src/Components/workWindow.jsx
+++ src/Components/workWindow.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import Chevron from "react-chevron";
 import BedmimeImage from "../assets/images/BedmimeTitle.png";
 import StormchaserImage from "../assets/images/StormchaserCover.png";
+import WebsiteImage from "../assets/images/WebsiteImage.png";
+import BlenderImage from "../assets/images/BlenderCollectionForWebsite.webp";
+import WorkTile from "./workTile";
 
 export default function WorkWindow({ workOpen, workOnClose }) {
   const [gamesOpen, setGamesOpen] = useState(false);
@@ -37,7 +40,7 @@ export default function WorkWindow({ workOpen, workOnClose }) {
           <hr className="stroke-black opacity-20" />
 
           <div
-            className={`overflow-y-auto font-outfit text-2xl pl-10 pr-10 pt-5 pb-20`}
+            className={`overflow-y-auto font-outfit text-2xl pl-10 pr-10 pb-20 pt-5`}
           >
             {/*Body of Work Window*/}
             <div>
@@ -53,140 +56,178 @@ export default function WorkWindow({ workOpen, workOnClose }) {
                   <Chevron direction={`${gamesOpen ? "up" : "down"}`} />
                 </div>
               </button>
-              <h1
-                className={`transition-all duration-300 ease-in-out ${
-                  gamesOpen
-                    ? "text-[1.2rem] opacity-100 mt-5 mb-2"
-                    : "text-[0rem] opacity-0 mt-0 mb-0"
-                }`}
-              >
-                All of my game projects can be found&nbsp;
-                <a href="https://philosofox.itch.io/" target="blank_">
-                  <span className="font-outfitMedium text-green-700 underline hover:text-green-900">
-                    here
-                  </span>
-                </a>
-                , but these are my favorites.
-              </h1>
-              <div
-                className={`grid grid-cols-1 transition-all duration-300 ease-in-out ${
-                  gamesOpen
-                    ? "opacity-100 gap-y-10"
-                    : "opacity-0 text-[0rem] mt-0 gap-y-0"
-                }`}
-              >
-                {/*Work Tile*/}
-                <div
-                  className={`transition-all duration-300 ease-in-out bg-workTileBg ${
-                    gamesOpen ? "w-180 h-64" : "w-0 h-0"
+              <WorkTile isOpen={gamesOpen}>
+                <h1
+                  className={`transition-all duration-300 ease-in-out ${
+                    gamesOpen
+                      ? "text-[1.2rem] opacity-100 mt-2 mb-2"
+                      : "text-[0rem] opacity-0"
                   }`}
                 >
-                  <div className={`flex align-middle self-center m-4`}>
-                    <img
-                      src={BedmimeImage}
-                      alt="BedmimeThumbnail"
-                      className={`self-center w-9/16 outline-5 outline-siteGreen transition-all duration-300 ease-in-out ${
-                        gamesOpen ? "w-9/16" : "w-0"
-                      }`}
-                    />
-                    <h1
-                      className={`ml-5 text-[1.3rem] ${
-                        gamesOpen
-                          ? "text-[1.3rem] opacity-100"
-                          : "text-[0rem] opacity-0"
-                      }`}
-                    >
-                      <i>Bedmime</i> was the first game I ever made. A smaller
-                      project meant to learn the basics of Unity, Bedmime has
-                      you holding back a silent beast with only a flashlight.
-                    </h1>
+                  All of my game projects can be found&nbsp;
+                  <a href="https://philosofox.itch.io/" target="blank_">
+                    <span className="font-outfitMedium text-green-700 underline hover:text-green-900">
+                      here
+                    </span>
+                  </a>
+                  , but these are my favorites.
+                </h1>
+                <div
+                  className={`grid grid-cols-1 transition-all duration-300 ease-in-out gap-y-10`}
+                >
+                  <div
+                    className={`transition-all duration-300 ease-in-out bg-workTileBg ${
+                      gamesOpen ? "max-w-180 max-h-64" : "max-w-0 max-h-0"
+                    }`}
+                  >
+                    <div className={`flex align-middle self-center m-4`}>
+                      <img
+                        src={BedmimeImage}
+                        alt="BedmimeThumbnail"
+                        className={`self-center outline-5 outline-siteGreen transition-all duration-300 ease-in-out ${
+                          gamesOpen ? "max-w-9/16" : "max-w-0"
+                        }`}
+                      />
+                      <h1
+                        className={`ml-5 ${
+                          gamesOpen ? "text-[1.3rem] opacity-100" : "hidden"
+                        }`}
+                      >
+                        <i>Bedmime</i> was the first game I ever made. A smaller
+                        project meant to learn the basics of Unity, Bedmime has
+                        you holding back a silent beast with only a flashlight.
+                      </h1>
+                    </div>
+                  </div>
+                  <div
+                    className={`transition-all duration-300 ease-in-out bg-workTileBg ${
+                      gamesOpen ? "max-w-180 max-h-64" : "max-w-0 max-h-0"
+                    }`}
+                  >
+                    <div className={`flex align-middle self-center m-4`}>
+                      <img
+                        src={StormchaserImage}
+                        alt="StormchaserThumbnail"
+                        className={`self-center outline-5 outline-siteGreen transition-all duration-300 ease-in-out ${
+                          gamesOpen ? "max-w-7/16" : "max-w-0"
+                        }`}
+                      />
+                      <h1
+                        className={`ml-5 ${
+                          gamesOpen ? "text-[1.3rem] opacity-100" : "hidden"
+                        }`}
+                      >
+                        <i>The Stormchaser</i> was a game made for the 2025 Phil
+                        Alexander Game Jam at Miami University. It was the first
+                        collaborative project I'd participated in, and taught me
+                        a lot about working with others.
+                      </h1>
+                    </div>
                   </div>
                 </div>
-                <div
-                  className={`transition-all duration-300 ease-in-out bg-workTileBg ${
-                    gamesOpen ? "w-180 h-64" : "w-0 h-0"
-                  }`}
+              </WorkTile>
+              <div className="mt-10">
+                {/*Dev Projects Section*/}
+                <button
+                  className={`bg-siteGreen text-white pt-2 pb-2 pl-10 pr-10 rounded-2xl cursor-pointer`}
+                  onClick={() => {
+                    setProjectsOpen(!projectsOpen);
+                  }}
                 >
-                  <div className={`flex align-middle self-center m-4`}>
-                    <img
-                      src={StormchaserImage}
-                      alt="StormchaserThumbnail"
-                      className={`self-center outline-5 outline-siteGreen transition-all duration-300 ease-in-out ${
-                        gamesOpen ? "w-7/16" : "w-0"
-                      }`}
-                    />
-                    <h1
-                      className={`ml-5 text-[1.3rem] ${
-                        gamesOpen
-                          ? "text-[1.3rem] opacity-100"
-                          : "text-[0rem] opacity-0"
+                  <div className="flex gap-1 items-center">
+                    Dev Projects
+                    <Chevron direction={`${projectsOpen ? "up" : "down"}`} />
+                  </div>
+                </button>
+                <WorkTile isOpen={projectsOpen}>
+                  <div
+                    className={`grid grid-cols-1 justify-items-center transition-all duration-300 ease-in-out ${
+                      projectsOpen
+                        ? "opacity-100 mt-5 gap-y-10"
+                        : "opacity-0 text-[0rem] mt-0 gap-y-0"
+                    }`}
+                  >
+                    <div
+                      className={`transition-all duration-300 ease-in-out bg-workTileBg ${
+                        projectsOpen ? "max-w-180 max-h-64" : "max-w-0 max-h-0"
                       }`}
                     >
-                      <i>The Stormchaser</i> was a game made for the 2025 Phil
-                      Alexander Game Jam at Miami University. It was the first
-                      collaborative project I have participated in, and helped
-                      me learn a lot about working with others.
-                    </h1>
+                      <div className={`flex align-middle self-center m-4`}>
+                        <img
+                          src={WebsiteImage}
+                          alt="Image of this Website /(o-o)\"
+                          className={`self-center outline-5 outline-siteGreen transition-all duration-300 ease-in-out ${
+                            projectsOpen ? "max-w-31/64" : "max-w-0"
+                          }`}
+                        />
+                        <h1
+                          className={`ml-5 ${
+                            projectsOpen
+                              ? "text-[1.3rem] opacity-100"
+                              : "hidden"
+                          }`}
+                        >
+                          Currently, This website is the only non-game based
+                          project I have fully developed. I have a few more
+                          projects cooking up in the background focused on
+                          assisting me academically, as well as some "for-fun"
+                          devlopments.
+                        </h1>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                </WorkTile>
               </div>
-            </div>
-            <div className="mt-10">
-              {/*Dev Projects Section*/}
-              <button
-                className={`bg-siteGreen text-white pt-2 pb-2 pl-10 pr-10 rounded-2xl cursor-pointer`}
-                onClick={() => {
-                  setProjectsOpen(!projectsOpen);
-                }}
-              >
-                <div className="flex gap-1 items-center">
-                  Projects
-                  <Chevron direction={`${projectsOpen ? "up" : "down"}`} />
-                </div>
-              </button>
-              <div
-                className={`grid grid-cols-2 justify-items-center transition-all duration-300 ease-in-out ${
-                  projectsOpen
-                    ? "opacity-100 mt-8 gap-y-10"
-                    : "opacity-0 text-[0rem] mt-0 gap-y-0"
-                }`}
-              >
-                <div
-                  className={`transition-all duration-300 ease-in-out bg-workTileBg ${
-                    projectsOpen ? "w-70 h-75" : "w-0 h-0"
-                  }`}
+              <div className="mt-10">
+                {/*Other Stuff Section*/}
+                <button
+                  className={`bg-siteGreen text-white pt-2 pb-2 pl-10 pr-10 rounded-2xl cursor-pointer`}
+                  onClick={() => {
+                    setOtherOpen(!otherOpen);
+                  }}
                 >
-                  <div className={`flex align-middle self-center m-4`}></div>
-                </div>
-                <div
-                  className={`transition-all duration-300 ease-in-out bg-workTileBg ${
-                    projectsOpen ? "w-70 h-75" : "w-0 h-0"
-                  }`}
-                ></div>
-              </div>
-            </div>
-            <div className="mt-10">
-              {/*Other Stuff Section*/}
-              <button
-                className={`bg-siteGreen text-white pt-2 pb-2 pl-10 pr-10 rounded-2xl cursor-pointer`}
-                onClick={() => {
-                  setOtherOpen(!otherOpen);
-                }}
-              >
-                <div className="flex gap-1 items-center">
-                  Other Stuff
-                  <Chevron direction={`${otherOpen ? "up" : "down"}`} />
-                </div>
-              </button>
-              <div
-                className={`grid grid-cols-3 justify-items-center transition-all duration-300 ease-in-out ${
-                  otherOpen
-                    ? "opacity-100 mt-8 gap-y-10"
-                    : "opacity-0 text-[0rem] mt-0 gap-y-0"
-                }`}
-              >
-                <h1>ge</h1>
-                <h1>gh</h1>
+                  <div className="flex gap-1 items-center">
+                    Other Stuff
+                    <Chevron direction={`${otherOpen ? "up" : "down"}`} />
+                  </div>
+                </button>
+                <WorkTile isOpen={otherOpen}>
+                  <div
+                    className={`grid grid-cols-1 justify-items-center transition-all duration-300 ease-in-out ${
+                      otherOpen
+                        ? "opacity-100 mt-5 gap-y-10"
+                        : "opacity-0 text-[0rem] mt-0 gap-y-0"
+                    }`}
+                  >
+                    <div
+                      className={`transition-all duration-300 ease-in-out bg-workTileBg ${
+                        otherOpen ? "max-w-180 max-h-58" : "max-w-0 max-h-0"
+                      }`}
+                    >
+                      <div className={`flex align-middle self-center m-4`}>
+                        <img
+                          src={BlenderImage}
+                          alt="Blender Project Images"
+                          loading="lazy"
+                          className={`self-center outline-5 max-w-31/64 outline-siteGreen`}
+                        />
+                        <h1
+                          className={`ml-5 transition-all duration-300 ease-in-out ${
+                            otherOpen
+                              ? "text-[1.3rem] opacity-100"
+                              : "text-0 hidden"
+                          }`}
+                        >
+                          Though I focus more on the programming/back-end side
+                          of development, I love the artsy side of things as
+                          well. 3D modeling with programs like blender is not
+                          only a hobby of mine, but a skill I find necessary to
+                          improve.
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </WorkTile>
               </div>
             </div>
           </div>
