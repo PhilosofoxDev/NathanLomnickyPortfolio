56d5512c05d1d57e6f7b58113e841ec76e1acad5
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-09-09 09:15:16 -0400

    update Index.jsx file

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./Css Files/NavBar.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <>
+      <App />
+    </>
+  </React.StrictMode>
+);
