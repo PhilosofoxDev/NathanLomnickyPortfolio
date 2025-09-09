0bca2c060a4252d169fbda3e03d8f6cb9148cedc
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2025-09-09 09:32:13 -0400

    Fixes accounting for vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import "tailwindcss";
 import Shortcuts from "./Components/desktopShortcuts";
 import Ground from "./Components/bottomGround";
 import Background from "./Components/background";
 import React, { useState } from "react";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   const [isDark, setIsDark] = useState(false);
 
   return (
-    <div className={`${isDark ? "dark" : ""}`}>
-      <div className="select-none z-100">
-        <div className="flex absolute items-end justify-start w-screen h-screen">
-          <Ground Mode={isDark ? true : false} />
+    <Router>
+      <div className={`${isDark ? "dark" : ""}`}>
+        <div className="select-none z-100">
+          <div className="flex absolute items-end justify-start w-screen h-screen">
+            <Ground Mode={isDark ? true : false} />
+          </div>
+          <div>
+            <Shortcuts Mode={isDark} setMode={setIsDark} />
+          </div>
         </div>
-        <div>
-          <Shortcuts Mode={isDark} setMode={setIsDark} />
-        </div>
+        <Background Mode={isDark} />
       </div>
-      <Background Mode={isDark} />
-    </div>
+    </Router>
   );
 }
 
